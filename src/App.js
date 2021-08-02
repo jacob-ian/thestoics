7574e8d294c7738b7a5cd22dfb326efeabebd7eb
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-08-02 15:43:00 +1000

    feat(App): Add Switch with Routes
    - Connect Routes to components
    - Handle 404
    - Add Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, useLocation } from "react-router-dom";
+import Stoic from "./components/Stoic";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NotFound from "./components/NotFound";
+import { getStoics } from "./api/stoics";
 
-function App() {
+function App(props) {
+  const stoics = getStoics();
+
   return (
     <div className="App">
-      <Switch>
-        <Route path="/stoic/:name"></Route>
-        <Route path="/"></Route>
-      </Switch>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact={true} path="/stoic/:id">
+            <Stoic />
+          </Route>
+          <Route exact={true} path="/">
+            <Home stoics={stoics} />
+          </Route>
+          <Route exact={true} path="/404">
+            <NotFound />
+          </Route>
+          <Route path="*">
+            <Redirect
+              to={{
+                pathname: "/404",
+                search: `path=${useLocation().pathname}`,
+              }}
+            />
+          </Route>
+        </Switch>
+      </main>
+      <Footer />
     </div>
   );
 }
