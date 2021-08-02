3f173fd098fa6fc09ae66c30c0b9a236a5ad7ef1
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-08-02 15:45:12 +1000

    fix(Stoic): Add API call handling
    - Handle errors and displaying of individual stoic

--- src/components/Stoic.js
+++ src/components/Stoic.js
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+import { Redirect, useLocation, useParams } from "react-router-dom";
+import { getStoicById } from "../api/stoics";
+import useTitle from "../hooks/useTitle";
 import "./Stoic.css";
 
-export default function Stoic(props) {}
+export default function Stoic() {
+  const [stoic, setStoic] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  const { pathname } = useLocation();
+
+  useTitle(`${stoic?.name}`);
+
+  useEffect(() => {
+    let error;
+    function getStoic() {
+      try {
+        return getStoicById(id);
+      } catch (err) {
+        error = err;
+      }
+    }
+
+    const stoic = getStoic();
+
+    setIsLoading(false);
+    setStoic(stoic);
+
+    if (error) {
+      setError(error);
+    }
+  }, [id]);
+
+  if (error && error.code === 404) {
+    return <Redirect to={{ pathname: "/404", search: `path=${pathname}` }} />;
+  } else if (isLoading) {
+    return <div></div>;
+  } else {
+    return <div className="stoic">{JSON.stringify(stoic)}</div>;
+  }
+}
