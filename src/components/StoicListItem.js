1fa8179d8af9013d66520801813348f8205cb263
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-08-03 15:54:04 +1000

    feat(Home): Build out StoicListItem
    - Create structure with years and name
    - Add styling

--- src/components/StoicListItem.js
+++ src/components/StoicListItem.js
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import { formatYears } from "../api/utils";
 import "./StoicListItem.css";
 
 export default function StoicListItem(props) {
-  const { id, name } = props;
+  const { id, name, birth, death } = props;
+  const years = formatYears(birth, death);
 
   return (
-    <li className="stoic-list-item">
-      <Link to={`/stoic/${id}`}>{name}</Link>
+    <li className="stoic-li">
+      <Link to={`/stoic/${id}`}>
+        <div className="stoic-li-years">{years}</div>
+        <div className="stoic-li-name">{name}</div>
+      </Link>
     </li>
   );
 }
