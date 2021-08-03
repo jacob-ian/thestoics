d6cd5c081942ee7c1584842ff5399870b8698435
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-08-03 15:55:16 +1000

    feat(Stoic): Flesh out Stoic info page
    - Add name as heading
    - Add years as a subtitle
    - Add a lorem paragraph for details
    - Add ReturnButton to go home

--- src/components/Stoic.js
+++ src/components/Stoic.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { Redirect, useLocation, useParams } from "react-router-dom";
 import { getStoicById } from "../api/stoics";
+import { formatYears } from "../api/utils";
 import useTitle from "../hooks/useTitle";
+import ReturnButton from "./ReturnButton";
 import "./Stoic.css";
 
 export default function Stoic() {
@@ -38,6 +40,19 @@ export default function Stoic() {
   } else if (isLoading) {
     return <div></div>;
   } else {
-    return <div className="stoic">{JSON.stringify(stoic)}</div>;
+    return (
+      <div className="stoic-container">
+        <h1>{stoic.name}</h1>
+        <p className="stoic-years">{formatYears(stoic.birth, stoic.death)}</p>
+        <p>
+          Commodo deserunt Lorem anim id non id laborum consectetur aliquip
+          consectetur tempor. Ut minim adipisicing velit enim minim incididunt
+          adipisicing enim in consequat velit qui in. Veniam mollit deserunt
+          duis esse non. Officia deserunt esse ea dolor veniam esse incididunt
+          voluptate. Adipisicing do cillum dolor aliqua.
+        </p>
+        <ReturnButton />
+      </div>
+    );
   }
 }
